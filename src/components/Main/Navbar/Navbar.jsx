1c9d5019f87de064a31e75e5cf4c804771d35077
Nazar Ashirov <newnewqwes@gmail.com>
2020-06-30 18:50:32 +0300

    grid4

--- src/components/Main/Navbar/Navbar.jsx
+++ src/components/Main/Navbar/Navbar.jsx
@@ -5,20 +5,20 @@ import Menu from './Menu/Menu';
 const Navbar = () => {
     return (
         <section className={s.section}>
-        <div className={s.imgName}>
-            <div className={s.avatar}>
-                <img src="https://qwes.pw/img/photo.jpg" alt="avatar"/>
+            <div className={s.imgName}>
+                <div className={s.avatar}>
+                    <img src="https://qwes.pw/img/photo.jpg" alt="avatar" />
+                </div>
+                <div className={s.name}>
+                    <h3>Аширов Назар</h3>
+                </div>
             </div>
-            <div className={s.name}>
-                <h3>Аширов Назар</h3>
+            <div className={s.progress__bar}>
+                <div>progress bar</div>
+                <div>progress bar</div>
+                <div>progress bar</div>
             </div>
-        </div>
-        <div className={s.progress__bar}>
-            <div>progress bar</div>
-            <div>progress bar</div>
-            <div>progress bar</div>
-        </div>
-        <Menu />
+            <Menu />
         </section>
     )
 }
