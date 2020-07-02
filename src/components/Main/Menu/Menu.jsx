572cb33a51f59c68d702ad07945f344f04ead400
Nazar Ashirov <newnewqwes@gmail.com>
2020-07-02 11:12:26 +0300

    disigne change

--- src/components/Main/Menu/Menu.jsx
+++ src/components/Main/Menu/Menu.jsx
@@ -1,12 +1,55 @@
 import React from 'react'
 import s from './Menu.module.scss'
 import MenuItem from './MenuItem/MenuItem'
+import { NavLink } from 'react-router-dom'
 
 const Menu = () => {
     return (
         <nav className={s.menu}>
+            <div className={s.imgSection}>
+                <div className={s.imgContainer}>
+                    <img src="https://qwes.pw/img/photo.jpg" alt="" />
+                </div>
+                <div className={s.imgTitle}>
+                    <MenuItem value="Аширов Назар" status="active" linkUrl="/Profile" />
+                </div>
+                <div className={s.imgStatusBar}>
+                    <div className={s.barContainer}>
+                        <div className={s.barTitle}>
+                            ПД.
+                        </div>
+                        <div className={s.bar}>
+                            <progress max="100" value="80" className={s.bar1}>
+                                <div class="progress-bar">
+                                </div>
+                            </progress>
+                        </div>
+                    </div>
+                    <div className={s.barContainer}>
+                        <div className={s.barTitle}>
+                            УР.
+                        </div>
+                        <div className={s.bar}>
+                            <progress max="100" value="60" className={s.bar2}>
+                                <div class="progress-bar">
+                                </div>
+                            </progress>
+                        </div>
+                    </div>
+                    <div className={s.barContainer}>
+                        <div className={s.barTitle}>
+                            АКТ.
+                        </div>
+                        <div className={s.bar}>
+                            <progress max="100" value="70" className={s.bar3}>
+                                <div class="progress-bar">
+                                </div>
+                            </progress>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <ul className={s.ul}>
-                <MenuItem value="Моя страница" status="active" linkUrl="/Profile" />
                 <MenuItem value="Новости" linkUrl="/News" />
                 <MenuItem value="Сообщения" linkUrl="/Dialogs" />
                 <MenuItem value="Чат" linkUrl="/Chat" />
--- src/components/Main/Pages/Profile/Profile.jsx
+++ src/components/Main/Pages/Profile/Profile.jsx
@@ -7,15 +7,7 @@ const Profile = () => {
         <section className={s.section}>
             <div className={s.wrapper}>
                 <div className={s.row}>
-                    <div className={s.imgSection}>
-                        <div className={s.imgContainer}>
-                            <img src="https://qwes.pw/img/photo.jpg" alt=""/>
-                        </div>
-                        <div className={s.imgButtonContainer}>
-                            <button>Редактировать</button>
-                            <button>...</button>
-                        </div>
-                    </div>
+                   
                     <div className={s.level}>Назар Аширов</div>
                     <div className={s.catalog}>catalog image</div>
                     <div className={s.post}>
