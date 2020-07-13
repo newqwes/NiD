652a7542f19fa67bc32d8dcc2fe154d6c1fb2594
Nazar Ashirov <newnewqwes@gmail.com>
2020-07-13 18:51:18 +0300

    edited queries and styles a little

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,72 +2,73 @@ import React from 'react'
 import s from './Menu.module.scss'
 import MenuItem from './MenuItem/MenuItem'
 import * as axios from 'axios'
-const Menu = (props) => {
 
-    props.addNewTime();
-    axios.get("https://www.nbrb.by/api/exrates/rates/145").then(respons => {
-        props.addNewExchangeRate(respons.data.Cur_OfficialRate);
-    })
-    setInterval(() => props.addNewTime(), 1000);
-
-    return (
-        <nav className={s.menu}>
-            <div className={s.imgSection}>
-                <div className={s.imgContainer}>
-                    <img src="https://cdn.iconscout.com/icon/free/png-512/avatar-370-456322.png" alt="" />
-                </div>
-                <div className={s.imgTitle}>
-                    <MenuItem value="Аширов Назар" status="active" linkUrl="/Profile" />
-                </div>
-                <div className={s.imgStatusBar}>
-                    <div className={s.barContainer}>
-                        <div className={s.barTitle}>
-                            ПД.
+class Menu extends React.Component {
+    componentDidMount() {
+        axios.get("https://www.nbrb.by/api/exrates/rates/145").then(respons => {
+            this.props.addNewExchangeRate(respons.data.Cur_OfficialRate);
+        })
+        setInterval(() => this.props.addNewTime(), 1000);
+    }
+    render() {
+        return (
+            <nav className={s.menu}>
+                <div className={s.imgSection}>
+                    <div className={s.imgContainer}>
+                        <img src="https://cdn.iconscout.com/icon/free/png-512/avatar-370-456322.png" alt="" />
+                    </div>
+                    <div className={s.imgTitle}>
+                        <MenuItem value="Аширов Назар" status="active" linkUrl="/Profile" />
+                    </div>
+                    <div className={s.imgStatusBar}>
+                        <div className={s.barContainer}>
+                            <div className={s.barTitle}>
+                                ПД.
+                            </div>
+                            <div className={s.bar}>
+                                <progress max="100" value="80" className={s.bar1}>
+                                    <div className={s.progressBar}>
+                                    </div>
+                                </progress>
+                            </div>
                         </div>
-                        <div className={s.bar}>
-                            <progress max="100" value="80" className={s.bar1}>
-                                <div className={s.progressBar}>
-                                </div>
-                            </progress>
+                        <div className={s.barContainer}>
+                            <div className={s.barTitle}>
+                                УР.
+                            </div>
+                            <div className={s.bar}>
+                                <progress max="100" value="60" className={s.bar2}>
+                                    <div className={s.progressBar}>
+                                    </div>
+                                </progress>
+                            </div>
+                        </div>
+                        <div className={s.barContainer}>
+                            <div className={s.barTitle}>
+                                АКТ.
+                            </div>
+                            <div className={s.bar}>
+                                <progress max="100" value="70" className={s.bar3}>
+                                    <div className={s.progressBar}>
+                                    </div>
+                                </progress>
+                            </div>
                         </div>
                     </div>
-                    <div className={s.barContainer}>
-                        <div className={s.barTitle}>
-                            УР.
-                        </div>
-                        <div className={s.bar}>
-                            <progress max="100" value="60" className={s.bar2}>
-                                <div className={s.progressBar}>
-                                </div>
-                            </progress>
-                        </div>
+                </div>
+                <ul className={s.ul}>
+                    {this.props.menuItemData.map(m => <MenuItem key={m.id} value={m.value} linkUrl={m.linkUrl} />)}
+                </ul>
+                <div className={s.infoWtapper}>
+                    <div className={s.titleTime}>
+                        <h2>{this.props.whatTimeNow}</h2>
                     </div>
-                    <div className={s.barContainer}>
-                        <div className={s.barTitle}>
-                            АКТ.
-                        </div>
-                        <div className={s.bar}>
-                            <progress max="100" value="70" className={s.bar3}>
-                                <div className={s.progressBar}>
-                                </div>
-                            </progress>
-                        </div>
+                    <div className={s.titleMoney}>
+                        <h2>1$ = {this.props.exchangeRate}руб.</h2>
                     </div>
                 </div>
-            </div>
-            <ul className={s.ul}>
-                {props.menuItemData.map(m => <MenuItem key={m.id} value={m.value} linkUrl={m.linkUrl} />)}
-            </ul>
-            <div className={s.infoWtapper}>
-                <div className={s.titleTime}>
-                    <h2>{props.whatTimeNow}</h2>
-                </div>
-                <div className={s.titleMoney}>
-                    <h2>1$ = {props.exchangeRate}руб.</h2>
-                </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 }
-
 export default Menu;
--- src/components/Pages/Users/Users.jsx
+++ src/components/Pages/Users/Users.jsx
@@ -3,62 +3,44 @@ import s from './Users.module.scss';
 import * as axios from 'axios';
 import photoUser from '../../../assets/images/hipster.svg'
 
-const Users = (props) => {
-    if (props.usersData.length === 0) {
+class Users extends React.Component {
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(respons => {
-            props.setUsers(respons.data.items)
+            this.props.setUsers(respons.data.items)
         })
-        // props.setUsers([
-
-        //     {
-        //         id: 0, fullName: 'Анна', status: 'Профессионально занимаюсь маникюром', avatarUrl: 'https://cdn.iconscout.com/icon/free/png-64/avatar-373-456325.png',
-        //         location: { country: 'Белерусь', city: 'Гродно' }, isSubscribe: false
-        //     },
-        //     {
-        //         id: 1, fullName: 'Майя', status: 'Домашние задания', avatarUrl: 'https://cdn3.iconfinder.com/data/icons/avatars-flat/33/woman_9-512.png',
-        //         location: { country: 'Туркменистан', city: 'Ашхабад' }, isSubscribe: true
-        //     },
-        //     {
-        //         id: 2, fullName: 'Азим', status: 'Учусь в школе', avatarUrl: 'https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png',
-        //         location: { country: 'Туркменистан', city: 'Ашхабад' }, isSubscribe: false
-        //     }
-
-        // ])
     }
 
-    let userContainer = props.usersData.map(n => {
+    render() {
         return (
-            <div key={n.id} className={s.userContainer}>
-                <div className={s.avatarWrapper}>
-                    <img className={s.imgAvatar} src={n.photos.small != null ? n.photos.small : photoUser} alt="" />
-                </div>
-                <div className={s.subscribeButtonWrapper}>
-                    {n.isSubscribe
-                        ? <button className={s.unSubscribeButton} onClick={() => props.unsubscribe(n.id)}>Отписаться</button>
-                        : <button className={s.subscribeButton} onClick={() => props.subscribe(n.id)}>Подписаться</button>}
+            <section className={s.usersWrapper}>{
+                this.props.usersData.map(n => {
+                    return (
+                        <div key={n.id} className={s.userContainer}>
+                            <div className={s.avatarWrapper}>
+                                <img className={s.imgAvatar} src={n.photos.small != null ? n.photos.small : photoUser} alt="" />
+                            </div>
+                            <div className={s.subscribeButtonWrapper}>
+                                {n.isSubscribe
+                                    ? <button className={s.unSubscribeButton} onClick={() => this.props.unsubscribe(n.id)}>Отписаться</button>
+                                    : <button className={s.subscribeButton} onClick={() => this.props.subscribe(n.id)}>Подписаться</button>}
 
-                </div>
-                <div className={s.fullNameWrapper}>
-                    <h3>{n.name}</h3>
-                </div>
-                <div className={s.statusWrapper}>
-                    <p className={s.statusP}>{n.status}</p>
-                </div>
-                <div className={s.country}>{"n.location.country"}</div>
-                <div className={s.city}>{"n.location.city"}</div>
-            </div>
+                            </div>
+                            <div className={s.fullNameWrapper}>
+                                <h3>{n.name}</h3>
+                            </div>
+                            <div className={s.statusWrapper}>
+                                <p className={s.statusP}>{n.status}</p>
+                            </div>
+                            <div className={s.country}>{"n.location.country"}</div>
+                            <div className={s.city}>{"n.location.city"}</div>
+                        </div>
+                    )
+                })
+            }
+            </section>
         )
-    })
-    return (
-        <section className={s.usersSection}>
-            <div className={s.usersWrapper}>
-                {userContainer}
-            </div>
-            <div className={s.buttonWrapper}>
-                <button>Далее</button>
-            </div>
-        </section>
-    )
+    }
 }
+
 
 export default Users
