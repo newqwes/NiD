7742641f90655152a84379dad0c45279d0c3ef8e
Nazar Ashyrov <newnewqwes@gmail.com>
2021-06-11 12:53:08 +0300

    Lesson:99. Try catch, router switch, classnames (#2)
    * Lesson 99
    
    * refactor: remove eslintcash
    
    Co-authored-by: Nazar Ashyrov <nazar.ashyrov@instinctools.com>

--- src/index.jsx
+++ src/index.jsx
@@ -9,13 +9,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 serviceWorker.unregister();
